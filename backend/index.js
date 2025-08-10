4904074fc7f0989c83fda2fb9b2b0ecb09484502
Suman De <sumande0414@gmail.com>
2025-08-10 17:34:50 +0530

    change port

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const pool = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
