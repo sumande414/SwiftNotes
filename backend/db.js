c56688e08ea33180a5e4597a994f8b1c2f706932
Suman De <sumande0414@gmail.com>
2025-08-10 12:59:20 +0530

    minor db changes

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
